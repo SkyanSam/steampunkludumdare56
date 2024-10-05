f7894435f3e7245b591bd535ce613d084a4698e2
SkyanSam <31390605+SkyanSam@users.noreply.github.com>
2024-10-05 16:05:26 -0400

    parry and attack signals

--- Player.gd
+++ Player.gd
@@ -12,32 +12,53 @@ extends CharacterBody2D
 @export var jump_speed: int = -3000
 @export var ground_pound_speed: int = 5000
 @export var gravity: int = 4000
-@export var falling_gravity: int = 6000;
+@export var falling_gravity: int = 6000
+
+@export var attack_cooldown: float = 0.01
+@export var parry_window: float = 0.01
+
+signal playerAttack
+
 var is_ground_pound = false
+var is_jump_last_frame = false
+var can_attack = true
+var can_parry = false
+@onready var rhythm_manager = get_tree().get_root().get_node(NodePath(get_tree().current_scene.name)).get_node("RhythmManager")
 #later apply a timer so that if the character is slightly off the ledge, they can still jump
 
 var vel = Vector2.ZERO
 
+enum AttackType {
+	NORMAL,
+	ONBEAT,
+	PARRY
+}
+
 func get_input(delta: float):
-	#if ui_right is pressed, velocity is increased to the right and sprite is flipped
 	if Input.is_action_pressed("ui_right"):
-		vel.x += accel * delta
-		#$AnimatedSprite2D.flip_h = false
-	#if ui_right is pressed, velocity is increased to the left and sprite is flipped
+		var cel = decel if vel.x < 0 else accel
+		vel.x += cel * delta
 	elif Input.is_action_pressed("ui_left"):
-		vel.x -= accel * delta
+		var cel = decel if vel.x > 0 else accel
+		vel.x -= cel * delta
 	elif vel.x > 0:
 		vel.x -= decel * delta
+		if (vel.x < 0): vel.x = 0
 	elif vel.x < 0:
 		vel.x += decel * delta
-		#$AnimatedSprite2D.flip_h = true
+		if (vel.x > 0): vel.x = 0
 	if (vel.x > speed):
 		vel.x = speed
 	if (vel.x < -speed):
 		vel.x = -speed
+func _process(delta):
+	pass
 
 func _physics_process(delta):
 	get_input(delta)
+	
+	if (is_on_floor()):
+		vel.y = 0
 	
 	if (vel.y > 0):
 		vel.y += gravity * delta
@@ -53,14 +74,37 @@ func _physics_process(delta):
 			vel.y = 0
 		else:
 			vel.y = ground_pound_speed
-	
-	set_velocity(vel)
-	set_up_direction(Vector2.UP)
-	move_and_slide()
-	vel = vel
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			vel.y = jump_speed
-	if Input.is_action_just_released("ui_up"):
+			is_jump_last_frame = true
+	if Input.is_action_just_released("ui_up") and is_jump_last_frame:
+		is_jump_last_frame = false
 		if vel.y < 0.0:
 			vel.y *= .2
+	
+	if Input.is_action_just_pressed("attack"):
+		if can_parry:
+			emit_signal("playerAttack", AttackType.PARRY, self)
+			can_parry = false
+		elif can_attack:
+			$AttackTimer.start(attack_cooldown)
+			can_attack = false
+			emit_signal("playerAttack", AttackType.ONBEAT if rhythm_manager.is_within_timing_window(delta) else AttackType.NORMAL, self)
+			print("player attack")
+	velocity = vel
+	set_up_direction(Vector2.UP)
+	move_and_slide()
+
+func _on_attack_timer_timeout() -> void:
+	can_attack = true
+
+func _on_enemy_attack():
+	if Input.is_action_just_pressed("attack"):
+		emit_signal("playerAttack", AttackType.PARRY, self)
+	else:
+		can_parry = true
+		$ParryTimer.start(parry_window)
+
+func _on_parry_timer_timeout() -> void:
+	can_parry = false
