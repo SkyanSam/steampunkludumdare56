c09581e13348de5fd91c65f083473587cf691c2c
Gabe Cargo <46631573+Nagasaki42069@users.noreply.github.com>
2024-10-07 18:55:54 -0400

    Add files via upload

---
+++ gearreverse.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+# Variables for gear rotation speed and direction
+var rotation_speed: float = 1.0
+var direction: int = 1 # 1 for clockwise, -1 for counterclockwise
+
+func _process(delta: float) -> void:
+    # Rotate the gear sprite
+    $Gear.rotation += (direction * rotation_speed * delta)*-1
