2957d07cd97c49be9ee044b61ed507ebd2a5943a
SkyanSam <31390605+SkyanSam@users.noreply.github.com>
2024-10-10 18:20:32 -0400

    player can now take damage from the ant

--- ActualAnt/ActualAnt.gd
+++ ActualAnt/ActualAnt.gd
@@ -7,7 +7,11 @@ extends Area2D
 var is_attacking = false
 var can_attack = true
 var speed_mod : int = 0
+var is_colliding_with_player = false
+@onready var player = get_node("/root/" + get_tree().current_scene.name + "/Player")
+var Player = load("res://Player.gd")
 func _ready() -> void:
+	player.connect("playerAttack", Callable(self, "_on_Player_attack"))
 	choose_phase()
 
 func _process(delta: float) -> void:
@@ -36,6 +40,7 @@ func attack():
 		can_attack = false
 		$AttackTimer.start(attack_cooldown)
 		$AnimatedSprite2D.play("attack")
+		player.take_damage(1)
 	
 func _on_timer_timeout() -> void:
 	choose_phase()
@@ -45,7 +50,16 @@ func _on_attack_timer_timeout() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "Player"):
+		is_colliding_with_player = true
 		if body.global_position.x - global_position.x > 0 and not $AnimatedSprite2D.flip_h:
 			attack()
 		if body.global_position.x - global_position.x < 0 and $AnimatedSprite2D.flip_h:
 			attack()
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		is_colliding_with_player = false
+
+func _on_Player_attack(_type, _player):
+	if is_colliding_with_player and (_type == Player.AttackType.NORMAL or _type == Player.AttackType.ONBEAT):
+		$HP.take_damage()
